97cf39c16274c66012ad48515eb07d1f08a4e529
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-06-26 17:38:04 -0400

    utils to check broken links #10

---
+++ utils/geturls.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+latest=$(cd ../formats; ls -1d * | grep -E "V[0-9]" | tail -1)
+urls=$(egrep -o -h 'https?://[^ ]+' ../formats/${latest}/*.sh | grep -o '^[^\[]*' | grep -v \${)
+
+./checkurls.sh ${urls}

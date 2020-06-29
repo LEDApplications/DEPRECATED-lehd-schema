e5ed920efe1c8fbd5a2a0eb57b00852424464c9c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-29 20:14:42 +0000

    Deploy LEDApplications/lehd-schema to github.com/LEDApplications/lehd-schema.git:gh-pages

--- utils/checkurls.sh
+++ utils/checkurls.sh
@@ -4,6 +4,6 @@ for url in $@
 do
     urlstatus=$(curl -o /dev/null --silent --head --write-out '%{http_code}' "$url" )
     if [ $urlstatus -ne 200 ] && [ $urlstatus -ne 302 ] && [ $urlstatus -ne 301 ]; then
-      echo "$url  $urlstatus"
+      echo "$url  (status: $urlstatus)"
     fi
 done

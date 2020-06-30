70a5cd3dced296ba87deba4134e6f5106703418e
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-06-30 17:35:11 -0400

    fix 2020 cip label #7

--- formats/V4.7.0/write_schemadoc.sh
+++ formats/V4.7.0/write_schemadoc.sh
@@ -522,11 +522,11 @@ include::label_cip_level.csv[]
 " >> $asciifile
 
 echo "
-==== 2020 CIP Codes
+==== CIP Codes
 ( link:label_cipcode.csv[] )
 
 CIP codes are sourced from the https://nces.ed.gov/ipeds/cipcode/[National Center for Education Statistics (NCES), Integrated Postsecondary Education Data System (IPEDS)].
-Data are reported using 2010 CIP codes, for all years.
+Data are reported using 2020 CIP codes, for all years.
 In 4-digit CIP tabulations, select degree levels are collapsed to the 2-Digit CIP family, using custom codes (e.g., 01.XX, 02.XX, etc.).
 
 [width=\"90%\",format=\"csv\",cols=\"^1,<2,^1,^1,<6\",options=\"header\"]

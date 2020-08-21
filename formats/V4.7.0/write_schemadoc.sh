a2703758159d0b777069790e7e55c02830825186
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-08-21 11:13:59 -0400

    Improvement: remove 4-digit custom xx codes line #27

--- formats/V4.7.0/write_schemadoc.sh
+++ formats/V4.7.0/write_schemadoc.sh
@@ -530,7 +530,6 @@ echo "
 
 CIP codes are sourced from the https://nces.ed.gov/ipeds/cipcode/[National Center for Education Statistics (NCES), Integrated Postsecondary Education Data System (IPEDS)].
 Data are reported using 2020 CIP codes, for all years.
-In 4-digit CIP tabulations, select degree levels are collapsed to the 2-Digit CIP family, using custom codes (e.g., 01.XX, 02.XX, etc.).
 
 [width=\"90%\",format=\"csv\",cols=\"^1,<2,^1,^1,<6\",options=\"header\"]
 |===================================================
